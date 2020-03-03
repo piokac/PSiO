88eaf494ebedc35e040dad8c5c1aad92e6a29ca8
Piotr Kaczmarek <piokac@gmail.com>
2020-03-03 13:28:53 +0100

    lecture 1

---
+++ L1/przeszukiwanie/main.cpp
@@ -0,0 +1,64 @@
+﻿#include <cmath>
+#include <iostream>
+#include <vector>
+//using namespace std;
+int find_min(const std::vector<int> &tab_stl)
+{
+    int min;
+
+    min = tab_stl[0];
+    for (const int &el : tab_stl) {
+        if (el < min) {
+            min = el;
+        }
+    }
+    return min;
+}
+
+int count_elements(const std::vector<int> &tab_stl, int value)
+{
+    int counter = 0;
+    for (const int &el : tab_stl) {
+        if (el == value) {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+bool is_prime_number(int N)
+{
+    bool czy_podzielnik = false;
+    for (int i = 2; i < sqrt(N) && czy_podzielnik == false; i++) {
+        if (N % i == 0) {
+            czy_podzielnik = true;
+        }
+    }
+    return !czy_podzielnik;
+}
+
+int main()
+{
+    std::vector<int> tab_stl(100);
+    int min;
+    //inicjacja tablicy stl
+    for (size_t i = 0; i < tab_stl.size(); i++)
+        tab_stl[i] = rand() % 10;
+
+    //znajdywanie minimum
+    min = find_min(tab_stl);
+    std::cout << "Wartość minimalna: " << min << std::endl;
+
+    //zliczanie liczby elementów o danej wartości
+    int number_to_find = 3;
+    int count = count_elements(tab_stl, number_to_find);
+    std::cout << "Liczba " << number_to_find << " wystepuje: " << count << " razy" << std::endl;
+
+    //liczba pierwsza
+    std::cout << "Podaj liczbe:";
+    int number;
+    std::cin >> number;
+    std::cout << "liczba jest pierwsza: " << is_prime_number(number);
+
+    return 0;
+}
---
+++ L1/tablice_kontenery/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <fstream>
+#include <iostream>
+#include <vector>
+//using namespace std;
+
+int main()
+{
+    int tab[100];
+    std::vector<int> tab_stl(100);
+
+    for (int i = 0; i < 100; i++)
+        tab_stl[i] = std::rand();
+    for (std::vector<int>::iterator it = tab_stl.begin(); it != tab_stl.end(); it++)
+        std::cin >> *it;
+
+    //użycie pętli while do odczytu pliku linia po lini
+    std::fstream file("/home/piotr/license.txt");
+    if (file.is_open()) {
+        std::string text_line;
+        while (getline(file, text_line)) {
+            std::cout << text_line << std::endl;
+        }
+
+    } else {
+        std::cout << "file does not exists" << std::endl;
+    }
+    //użycie pętli do while do pobrania właściwej odpowiedzi od użytkownika
+    char answer;
+    do {
+        std::cout << "czy chcesz kontynuowac(t/n)??";
+        std::cin >> answer;
+    } while (!(answer == 't' || answer == 'T' || answer == 'n' || answer == 'N'));
+
+    if (answer == 't' || answer == 'T') {
+        std::cout << "opdowiedziales tak...." << std::endl;
+    } else {
+        std::cout << "odpowiedziales nie..." << std::endl;
+    }
+    return 0;
+}
