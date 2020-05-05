38cb10b7308f6d74efde26e1b409f72e454b8f75
Piotr Kaczmarek <piokac@gmail.com>
2020-05-05 14:01:10 +0200

    L09 examples (advanced data structure and algorithms - graphs

---
+++ L9/maze_BFS/main.cpp
@@ -0,0 +1,104 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+
+class Board
+{
+    struct Node
+    {
+        Node() : visited(false), symbol('?') {}
+        bool visited;
+        char symbol;
+    };
+
+    size_t rows_, cols_;
+    std::vector<std::vector<Node>> maze;
+
+public:
+    Board(size_t r, size_t c) : rows_(r), cols_(c), maze(r, std::vector<Node>(c)) {}
+    void update_row(size_t row_id, const std::string &row)
+    {
+        //        std::cerr << row << std::endl;
+        for (size_t i = 0; i < row.size(); i++) {
+            maze[row_id][i].symbol = row[i];
+        }
+    }
+    bool is_exit(size_t r, size_t c) const
+    {
+        return (r == 0 || c == 0 || r == rows_ - 1 || c == cols_ - 1);
+    }
+
+    std::vector<std::pair<size_t, size_t>> neighbours(size_t row, size_t col)
+    {
+        std::vector<std::pair<size_t, size_t>> res;
+        if (row > 0)
+            if (!maze[row - 1][col].visited && maze[row - 1][col].symbol != '#')
+                res.emplace_back(row - 1, col);
+        if (col > 0)
+            if (!maze[row][col - 1].visited && maze[row][col - 1].symbol != '#')
+                res.emplace_back(row, col - 1);
+        if (row < rows_ - 1)
+            if (!maze[row + 1][col].visited && maze[row + 1][col].symbol != '#')
+                res.emplace_back(row + 1, col);
+        if (col < cols_ - 1)
+            if (!maze[row][col + 1].visited && maze[row][col + 1].symbol != '#')
+                res.emplace_back(row, col + 1);
+
+        return res;
+    }
+    std::vector<std::pair<size_t, size_t>> find_exits(size_t pos_row, size_t pos_col)
+    {
+        std::vector<std::pair<size_t, size_t>> res;
+        //TODO: find exits algorithm
+        std::queue<std::pair<size_t, size_t>> q;
+        q.emplace(pos_row, pos_col); //dadaje węzeł (parę współrzędnych) do kolejki
+        do {
+            auto &[row, col] = q.front();
+            if (!maze[row][col].visited) {
+                maze[row][col].visited = true;
+                if (is_exit(row, col)) {
+                    res.emplace_back(row, col);
+                }
+                std::vector<std::pair<size_t, size_t>> nodes = neighbours(row, col);
+                for (auto el : nodes) { //dodaje wszystkie nieodwiedzone węzły sąsiadów do kolejki
+                    q.emplace(el);
+                }
+            }
+            q.pop(); //usuwa odwiedzony węzeł z kolejki
+
+        } while (q.size() != 0);
+
+        return res;
+    }
+};
+
+int main()
+{
+    int W;
+    int H;
+    std::cin >> W >> H;
+    std::cin.ignore();
+    int X;
+    int Y;
+    std::cin >> X >> Y;
+    std::cin.ignore();
+    Board maze(W, H);
+    for (int i = 0; i < H; i++) {
+        std::string R;
+        getline(std::cin, R);
+        maze.update_row(i, R);
+    }
+
+    std::vector<std::pair<size_t, size_t>> res = maze.find_exits(Y, X);
+
+    // Write an answer using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    std::cout << res.size() << std::endl;
+    sort(res.begin(), res.end());
+    for (auto &[r, c] : res) {
+        std::cout << r << " " << c << std::endl;
+    }
+}
---
+++ L9/money_change/main.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <list>
+#include <vector>
+using namespace std;
+bool change_greedy(const std::list<int> &denominations, int change_value, std::list<int> &solution)
+{
+    std::list<int> denomination_queue = denominations;
+    while (denomination_queue.size() > 0 && change_value > 0) {
+        int value = *denomination_queue.begin();
+        denomination_queue.pop_front();
+        if (change_value > value) {
+            solution.emplace_back(value);
+            change_value = change_value - value;
+        }
+    }
+    return (change_value == 0);
+}
+
+bool change_dfs(const std::list<int> &denominations, int change_value, std::list<int> &solution)
+{
+    std::list<int> denomination_queue = denominations;
+    while (denomination_queue.size() > 0 && change_value > 0) {
+        int value = *denomination_queue.begin();
+        denomination_queue.pop_front();
+
+        if (change_value - value == 0) {
+            solution.emplace_back(value);
+            return true;
+        }
+        if (change_value > value) {
+            bool res = change_dfs(denomination_queue, change_value - value, solution);
+            if (res == true) {
+                solution.emplace_back(value);
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    std::list<int> denomination = {5, 5, 2, 2, 2};
+    std::list<int> result;
+    if (change_dfs(denomination, 6, result)) {
+        std::cout << "result found: ";
+        for (const auto &el : result)
+            std::cout << el << ", ";
+
+    } else {
+        std::cout << "result not found" << endl;
+    }
+    return 0;
+}
