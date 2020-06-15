c765416265526c0456f78f2586c80c386c25881b
Piotr Kaczmarek <piokac@gmail.com>
2020-06-15 13:41:02 +0200

    L15 initial code with abstract TicTacToe engine class

---
+++ L15/TicTacToe_sfml_board/TicTacToeSFML/clients/tictactoeconsole.h
@@ -0,0 +1,37 @@
+﻿#ifndef TICTACTOECONSOLE_H
+#define TICTACTOECONSOLE_H
+#include "common_defs.h"
+#include "tictactoeabstractplayer.h"
+#include <iostream>
+class TicTacToeConsole : public TicTacToeAbstractPlayer
+{
+    char convert(Player p)
+    {
+        if (p == Player::cross)
+            return 'x';
+        if (p == Player::circle)
+            return 'o';
+
+        return '.';
+    }
+
+public:
+    TicTacToeConsole(size_t size, Player player) : TicTacToeAbstractPlayer(size, player) {}
+    virtual Position move()
+    {
+        int row, col;
+        std::cin >> row >> col;
+        return {row, col};
+    }
+    virtual void display()
+    {
+        for (const auto &row : board_) {
+            for (const auto &el : row) {
+                std::cout << convert(el);
+            }
+            std::cout << std::endl;
+        }
+    }
+};
+
+#endif // TICTACTOECONSOLE_H
